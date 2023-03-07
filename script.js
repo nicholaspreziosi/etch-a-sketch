0a35ff76ecf1a470185aa7b55ebd2b83c0ebfaaf
Nicholas Preziosi <108760764+nicholaspreziosi@users.noreply.github.com>
2023-03-07 13:53:24 -0800

    Add prompt, add CSS, and create grid

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-let size = 16;
-
+let dimensions = prompt("Square Dimensions?");
 
 function createSquare () {
     window.onload = function() {
-        for (let i = 0; i < size; i++) {
+        for (let i = 0; i < dimensions; i++) {
             const div = document.createElement('div');
-            const container = document.querySelector('.container');
+            const container = document.querySelector('#container');
             container.appendChild(div);
             div.classList.add('div');
+        };
+        const divs = document.querySelectorAll('.div');
+        for (let i = 0; i < divs.length; i++) {
+            divs[i].addEventListener('mouseover', () => {
+                divs[i].classList.add('red');
+            });
         };
     }
 };
 
 
  
-
-
 createSquare();
